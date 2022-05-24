ced58f3e34205bc358233ae69abcb0d9c3b97eae
nurivega <nuriavega.nv@gmail.com>
2022-05-24 19:47:44 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const path=require("path");
+const app=express();
+
+const publicPath=path.resolve(__dirname,'./public');
+app.use(express.static(publicPath));
+
+app.listen(3000,()=> {
+    console.log("Servidor corriendo en el puerto 3000")
+});
+
+
+app.get('/',function(req,res){
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+})
+
+app.get('/registro',function(req,res){
+    res.sendFile(path.resolve(__dirname,'./views/registro.html'));
+})
+
+app.get('/login',function(req,res){
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+})
